7806b929748016747755f68b40a60bd1d0f51cb4
Tomoyuki Nagasaka <t_nagasaka@satt.jp>
2024-02-24 16:41:25 +0900

    php question2

--- src/Station02/Practice.php
+++ src/Station02/Practice.php
@@ -7,6 +7,14 @@ class Practice
     public function main(): void
     {
         // ここにサンプルコードを記述
+        $a = 1;
+        $b = '1';
+        
+        if ($a == $b) { // 緩やかな比較にする
+            echo '等しい';
+        } else {
+            echo '等しくない';
+        }
     }
 }
 
--- src/Station02/Question.php
+++ src/Station02/Question.php
@@ -6,5 +6,20 @@ class Question
 {
     public function main(mixed $arg): string
     {
+        $result = "";
+        if($arg === 0){
+            $result = "zero";
+        }else if($arg === "1"){
+            $result = "foo";
+        }else if($arg === 1){
+            $result = "bar";
+        }else if($arg >= 2){
+            $result = "baz";
+        }else{
+            $result = "others";
+        }
+        // echo $result;
+        return $result;
     }
 }
+// (new Question())->main("1");
