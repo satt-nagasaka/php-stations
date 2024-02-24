11d2355916a9dcd67c87e3b4e184046473e35c07
Tomoyuki Nagasaka <t_nagasaka@satt.jp>
2024-02-24 17:58:09 +0900

    php question6

--- src/Station06/Practice3.php
+++ src/Station06/Practice3.php
@@ -7,6 +7,19 @@ class Practice3
     public function main(): void
     {
         // ここにサンプルコードを記述
+        $array = [1, 2, 3, 4, 5];
+
+        array_pop($array);
+        print_r($array);
+        array_shift($array);
+        print_r($array);
+        array_splice($array, 2, 1);
+        print_r($array);
+
+        $array = [1, 2, 3, 4, 5];
+        // 配列の 0 番目の要素から数えて 3 つ目までを削除して、代わりに [6, 7] を挿入する
+        array_splice($array, 0, 3, [6, 7]);
+        print_r($array);
     }
 }
 
--- src/Station06/Question.php
+++ src/Station06/Question.php
@@ -6,5 +6,13 @@ class Question
 {
     public function main(): array
     {
+        $array = ['red', 'blue', 'yellow'] ;
+        array_splice($array,2,1);
+        array_unshift($array,"white","black");
+        array_splice($array,3,0,"green");
+
+        // print_r($array);
+        return $array;
     }
 }
+// (new Question())->main();
