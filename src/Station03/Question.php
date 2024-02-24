0903df5a93ae427e4250878b77a2a7dca5f91296
Tomoyuki Nagasaka <t_nagasaka@satt.jp>
2024-02-24 16:53:05 +0900

    php question3

--- src/Station03/Question.php
+++ src/Station03/Question.php
@@ -6,5 +6,19 @@ class Question
 {
     public function main(mixed $arg): string
     {
+        $result = "";
+        switch($arg){
+            case $arg===1:
+                $result = "りんご";
+                break;
+            case $arg===2:
+            case $arg===3:
+                $result = "みかん";
+                break;
+            default:
+                $result = "さくらんぼ";
+                break;
+        }
+        return $result;
     }
 }
