c8a67ad4b6fceca2bf5e9a07e742733975de2c4c
Tomoyuki Nagasaka <t_nagasaka@satt.jp>
2024-02-24 17:12:03 +0900

    php question5

--- src/Station05/Question.php
+++ src/Station05/Question.php
@@ -6,5 +6,18 @@ class Question
 {
     public function main(): int
     {
+        $result = 0;
+        $array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        foreach ($array as $value) {
+            if (($value % 4) > 2) {
+                continue;
+            }else if($value >= 8){
+                break;
+            }
+            $result = $result + $value;
+        }
+        // echo $result;
+        return $result;
     }
 }
