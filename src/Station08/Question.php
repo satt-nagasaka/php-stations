e3b05982fc0f26345275dbe25ac839bbff246eef
Tomoyuki Nagasaka <t_nagasaka@satt.jp>
2024-02-24 19:18:02 +0900

    php question8

--- src/Station08/Question.php
+++ src/Station08/Question.php
@@ -6,5 +6,15 @@ class Question
 {
     public function main(): array
     {
+        $resultArray = [
+            ["アザラシ","アライグマ"],
+            ["ウサギ","ウシ","ウマ"],
+            ["オオカミ","オットセイ"]
+        ];
+        $resultArray[2] = $resultArray[1];
+        $resultArray[1] = ["イヌ","イルカ"];
+        // print_r($resultArray);
+        return $resultArray;
     }
 }
+// (new Question())->main();
