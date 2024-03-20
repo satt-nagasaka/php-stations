0df5c2c4105e04490fc3c6669f1d64ddc2877359
Tomoyuki Nagasaka <t_nagasaka@satt.jp>
2024-03-20 23:01:28 +0900

    php question18

--- src/Station18/Practice/Practice.php
+++ src/Station18/Practice/Practice.php
@@ -1,12 +1,18 @@
 <?php
+
 namespace Src\Station18\Practice;
-class Practice{
-public function main(): void{
-$test = [
-1,2,3,
-];
-if ($test) {echo 'String';}
-echo 'Other';
-}
+
+class Practice
+{
+    public function main(): void
+    {
+        $test = [
+        1,2,3,
+        ];
+        if ($test) {
+            echo 'String';
+        }
+        echo 'Other';
+    }
 }
 (new Practice())->main();
--- src/Station18/Question/.php-cs-fixer.php
+++ src/Station18/Question/.php-cs-fixer.php
@@ -5,6 +5,7 @@ $finder = PhpCsFixer\Finder::create()->in(__DIR__);
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
+        'trailing_comma_in_multiline' => true,// この一行を追加
     ])
     ->setUsingCache(false)
     ->setFinder($finder);
